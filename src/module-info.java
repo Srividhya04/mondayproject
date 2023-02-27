f6b0b97755704a8431f460599c8d42cec49b0517
Surendar <Surendar@192.168.1.5>
2023-02-27 17:22:38 +0530

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Surendar
+ *
+ */
+module Railwayticket {
+}
---
+++ src/railwayticket/RailwayTicket.java
@@ -0,0 +1,50 @@
+package railwayticket;
+
+import java.util.Scanner;
+
+public class RailwayTicket {
+	String name;
+	String coach;
+	Long mobno;
+	int amt;
+	int totalamt;
+	
+	void accept () {
+		Scanner scanner= new Scanner (System.in);
+		System.out.println("Enter name:");
+		name = scanner.nextLine();
+		System.out.println("Enter coach:");
+		coach = scanner.nextLine();
+		System.out.println("Enter mobno:");
+		mobno = scanner. nextLong();
+		System.out.println("Enter amt: ");
+		amt= scanner.nextInt();		
+	}
+	void update() {
+		if (coach.equals("1Ac")) {
+			totalamt=amt+700;
+		}else if (coach.equals("2AC")) {
+			totalamt = amt+500;
+		}else if  (coach.equals("3AC")) {
+			totalamt= amt+250;
+		}else if (coach.equals("sleeper")) {
+			totalamt = amt;
+		}
+	}
+	void display () {
+		System.out.println("\t\t\tTicket Details");
+		System.out.println("Name:" + name);
+		System.out.println("coach:" +coach);
+		System.out.println("Mobile Number:"+ mobno);
+		System.out.println("Total amount:"+ totalamt);
+		
+	}
+	public static void main (String args[]) {
+		RailwayTicket railwayTicket = new RailwayTicket();
+		railwayTicket.accept();
+		railwayTicket.update();
+		railwayTicket.display();
+	}
+}
+
+		
